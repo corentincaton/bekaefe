1996b872e87b7d59b689b3f4bbab4193a2646bf9
Corentin Caton <corentincaton@gmail.com>
2023-02-20 19:19:25 +0100

    Add hover

--- js/index.js
+++ js/index.js
@@ -9,6 +9,11 @@ function readyFn(jQuery) {
         section.find(".color").eq(index).addClass("selected");
         section.find(".picture").eq(index).addClass("selected");
     });
+
+    $("[data-anchor]").click(function () {
+        let target = $(this).data("anchor");
+        $('html,body').animate({scrollTop: $("#" + target).offset().top},'slow');
+    });
 }
 
 $(document).ready(readyFn);
