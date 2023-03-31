4b84e3ed43e715b293555d8f8614184f8c245ac9
Corentin Caton <corentincaton@gmail.com>
2023-03-31 14:51:35 +0200

    Add tiles

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 function readyFn(jQuery) {
     $("#products .color").click(function () {
-        console.log($(this));
         let section = $(this).closest(".split-columns");
-        console.log(section);
         let index = section.find(".color").index($(this));
         section.find(".color").removeClass("selected");
         section.find(".picture").removeClass("selected");
         section.find(".color").eq(index).addClass("selected");
         section.find(".picture").eq(index).addClass("selected");
+    });
+
+    $(".materials .material").click(function name() {
+        let index = $(".materials .material").index($(this));
+        $(".materials .material, .materials .description").removeClass("selected");
+        $(".materials .material").eq(index).addClass("selected");
+        $(".materials .description").eq(index).addClass("selected");
     });
 
     $("[data-anchor]").click(function () {
